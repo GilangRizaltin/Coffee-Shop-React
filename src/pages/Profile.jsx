2f7232631d69a2de451e7011ad9bbfefad283921
GilangRizaltin <gilangmrizaltin@gmail.com>
2024-01-23 20:27:41 +0700

    update profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Header from "../components/Header";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import { updateDataUser, getUser } from '../https/profile';
 import { useSelector } from 'react-redux';
 
